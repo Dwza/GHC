6cf17076323c082b4cbfee40bac84522b1f6b989
Dwayne Sharp <info@pro-wis.de>
2022-04-26 22:19:08 +0200

    initial commit

---
+++ UI/config.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'config.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.4
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLineEdit, QMainWindow, QPushButton,
+    QSizePolicy, QWidget)
+
+class Ui_QFrmConfig(object):
+    def setupUi(self, QFrmConfig):
+        if not QFrmConfig.objectName():
+            QFrmConfig.setObjectName(u"QFrmConfig")
+        QFrmConfig.resize(247, 225)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(QFrmConfig.sizePolicy().hasHeightForWidth())
+        QFrmConfig.setSizePolicy(sizePolicy)
+        self.centralwidget = QWidget(QFrmConfig)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.edit_token_1 = QLineEdit(self.centralwidget)
+        self.edit_token_1.setObjectName(u"edit_token_1")
+        self.edit_token_1.setGeometry(QRect(30, 10, 51, 20))
+        self.edit_token_1.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_2 = QLineEdit(self.centralwidget)
+        self.edit_token_2.setObjectName(u"edit_token_2")
+        self.edit_token_2.setGeometry(QRect(30, 30, 51, 20))
+        self.edit_token_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_3 = QLineEdit(self.centralwidget)
+        self.edit_token_3.setObjectName(u"edit_token_3")
+        self.edit_token_3.setGeometry(QRect(30, 50, 51, 20))
+        self.edit_token_3.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_4 = QLineEdit(self.centralwidget)
+        self.edit_token_4.setObjectName(u"edit_token_4")
+        self.edit_token_4.setGeometry(QRect(30, 70, 51, 20))
+        self.edit_token_4.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_5 = QLineEdit(self.centralwidget)
+        self.edit_token_5.setObjectName(u"edit_token_5")
+        self.edit_token_5.setGeometry(QRect(30, 90, 51, 20))
+        self.edit_token_5.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_6 = QLineEdit(self.centralwidget)
+        self.edit_token_6.setObjectName(u"edit_token_6")
+        self.edit_token_6.setGeometry(QRect(30, 110, 51, 20))
+        self.edit_token_6.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_7 = QLineEdit(self.centralwidget)
+        self.edit_token_7.setObjectName(u"edit_token_7")
+        self.edit_token_7.setGeometry(QRect(30, 130, 51, 20))
+        self.edit_token_7.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_8 = QLineEdit(self.centralwidget)
+        self.edit_token_8.setObjectName(u"edit_token_8")
+        self.edit_token_8.setGeometry(QRect(30, 150, 51, 20))
+        self.edit_token_8.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_9 = QLineEdit(self.centralwidget)
+        self.edit_token_9.setObjectName(u"edit_token_9")
+        self.edit_token_9.setGeometry(QRect(30, 170, 51, 20))
+        self.edit_token_9.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_10 = QLineEdit(self.centralwidget)
+        self.edit_token_10.setObjectName(u"edit_token_10")
+        self.edit_token_10.setGeometry(QRect(30, 190, 51, 20))
+        self.edit_token_10.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_g_1 = QLineEdit(self.centralwidget)
+        self.edit_token_g_1.setObjectName(u"edit_token_g_1")
+        self.edit_token_g_1.setGeometry(QRect(170, 10, 41, 20))
+        self.edit_token_g_1.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_g_2 = QLineEdit(self.centralwidget)
+        self.edit_token_g_2.setObjectName(u"edit_token_g_2")
+        self.edit_token_g_2.setGeometry(QRect(170, 30, 41, 20))
+        self.edit_token_g_2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_g_3 = QLineEdit(self.centralwidget)
+        self.edit_token_g_3.setObjectName(u"edit_token_g_3")
+        self.edit_token_g_3.setGeometry(QRect(170, 50, 41, 20))
+        self.edit_token_g_3.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_g_4 = QLineEdit(self.centralwidget)
+        self.edit_token_g_4.setObjectName(u"edit_token_g_4")
+        self.edit_token_g_4.setGeometry(QRect(170, 70, 41, 20))
+        self.edit_token_g_4.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.edit_token_g_5 = QLineEdit(self.centralwidget)
+        self.edit_token_g_5.setObjectName(u"edit_token_g_5")
+        self.edit_token_g_5.setGeometry(QRect(170, 90, 41, 20))
+        self.edit_token_g_5.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(160, 140, 56, 51))
+        QFrmConfig.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(QFrmConfig)
+
+        QMetaObject.connectSlotsByName(QFrmConfig)
+    # setupUi
+
+    def retranslateUi(self, QFrmConfig):
+        QFrmConfig.setWindowTitle(QCoreApplication.translate("QFrmConfig", u"Config Window", None))
+        self.edit_token_1.setText(QCoreApplication.translate("QFrmConfig", u"12,5", None))
+        self.edit_token_2.setText(QCoreApplication.translate("QFrmConfig", u"37,5", None))
+        self.edit_token_3.setText(QCoreApplication.translate("QFrmConfig", u"75", None))
+        self.edit_token_4.setText(QCoreApplication.translate("QFrmConfig", u"200", None))
+        self.edit_token_5.setText(QCoreApplication.translate("QFrmConfig", u"250", None))
+        self.edit_token_6.setText(QCoreApplication.translate("QFrmConfig", u"300", None))
+        self.edit_token_7.setText(QCoreApplication.translate("QFrmConfig", u"700", None))
+        self.edit_token_8.setText(QCoreApplication.translate("QFrmConfig", u"800", None))
+        self.edit_token_9.setText(QCoreApplication.translate("QFrmConfig", u"900", None))
+        self.edit_token_10.setText(QCoreApplication.translate("QFrmConfig", u"1000", None))
+        self.edit_token_g_1.setText(QCoreApplication.translate("QFrmConfig", u"2", None))
+        self.edit_token_g_2.setText(QCoreApplication.translate("QFrmConfig", u"2", None))
+        self.edit_token_g_3.setText(QCoreApplication.translate("QFrmConfig", u"3", None))
+        self.edit_token_g_4.setText(QCoreApplication.translate("QFrmConfig", u"4", None))
+        self.edit_token_g_5.setText(QCoreApplication.translate("QFrmConfig", u"5", None))
+        self.pushButton.setText(QCoreApplication.translate("QFrmConfig", u"OK", None))
+    # retranslateUi
+
---
+++ main.py
@@ -0,0 +1,195 @@
+import yaml
+from PySide6.QtWidgets import QApplication, QMainWindow, QComboBox
+from UI.ghc import Ui_QFrmMain
+import math
+from os.path import exists
+from UI.config import Ui_QFrmConfig
+
+
+class FrmMain(QMainWindow, Ui_QFrmMain):
+
+    totalTokens = 0
+    cfgFile = 'ghc.yml'
+    cfg = {}
+    saveOnStart = False
+
+    def __init__(self):
+        super(FrmMain, self).__init__()
+        self.setupUi(self)
+        self.setFixedSize(self.size())
+        self.setWindowTitle('Great Hall Calculator - Level 0/240')
+        self.loadConfigs()
+        self.loadData()
+        self.bindHandlers()
+        self.configUi = Ui_QFrmConfig()
+        self.setFocus()
+        
+    """
+    def openConfigWindow(self):
+        self.window = QMainWindow()
+        self.ui = Ui_QFrmConfig()
+        self.ui.setupUi(self.window)
+        self.window.show()
+    """
+
+    @staticmethod
+    def createYaml(file):
+        stdConfig = {
+            "settings": {
+                "load_on_start": False,
+            },
+            "configs": {
+                "cb_stage": 1,
+                "tokens": 0,
+                "maxNeededTokens": 102600,
+                "sections": ['m', 's', 'f', 'v'],
+                "stageTokens": [2, 2, 3, 4, 5],
+                "subFromLvl": [12.5, 50, 125, 325, 575, 875, 1575, 2375, 3275, 4275],
+                "totalTokens": 102600
+            },
+            "ranks": {
+                "cb_m_1": 0,
+                "cb_m_2": 0,
+                "cb_m_3": 0,
+                "cb_m_4": 0,
+                "cb_m_5": 0,
+                "cb_m_6": 0,
+                "cb_s_1": 0,
+                "cb_s_2": 0,
+                "cb_s_3": 0,
+                "cb_s_4": 0,
+                "cb_s_5": 0,
+                "cb_s_6": 0,
+                "cb_f_1": 0,
+                "cb_f_2": 0,
+                "cb_f_3": 0,
+                "cb_f_4": 0,
+                "cb_f_5": 0,
+                "cb_f_6": 0,
+                "cb_v_1": 0,
+                "cb_v_2": 0,
+                "cb_v_3": 0,
+                "cb_v_4": 0,
+                "cb_v_5": 0,
+                "cb_v_6": 0,
+            }
+        }
+        with open(file, 'w') as ghd:
+            yaml.dump(stdConfig, ghd)
+
+    @staticmethod
+    def setCBColor(cb, rank):
+        color = "none"
+        if rank > 0:
+            color = "#d65d00"
+        if rank > 3:
+            color = "#0f80bd"
+        if rank > 6:
+            color = "#ffd103"
+
+        cb.setStyleSheet("border: 2px solid " + color + ";font-weight: bold")
+
+    @staticmethod
+    def toInt(v):
+        return v != '' and int(v) or 0
+
+    def loadConfigs(self):
+        if not exists(self.cfgFile):
+            self.createYaml(self.cfgFile)
+            print(self.cfgFile + " file has been created since there was no file before.")
+
+        with open(self.cfgFile, "r") as cfg:
+            self.cfg = yaml.load(cfg, Loader=yaml.FullLoader)
+
+        self.totalTokens = self.cfg['configs']['totalTokens']
+
+    def saveData(self):
+        s = 0
+        for affinity in self.cfg['configs']['sections']:
+            for cbId in range(6):
+                cbName = "cb_" + affinity + "_" + str(cbId+1)
+                cb = self.findChild(QComboBox, cbName)
+                v = self.toInt(cb.currentIndex())
+                self.cfg['ranks'][cbName] = v
+                s = s + v
+
+        self.cfg['configs']['cb_stage'] = self.toInt(self.cb_stage.currentText())
+        self.cfg['configs']['tokens'] = self.toInt(self.sb_tokens.text())
+        self.setWindowTitle('Great Hall Calculator - Level ' + str(s) + '/240')
+
+        with open(self.cfgFile, 'w') as cfg:
+            yaml.dump(self.cfg, cfg)
+
+    def loadData(self):
+        s = 0
+        sections = self.cfg['configs']['sections']
+        for affinity in sections:
+            for cbId in range(6):
+                cbName = "cb_" + affinity + "_" + str(cbId+1)
+                cb = self.findChild(QComboBox, cbName)
+                v = self.cfg['ranks'][cbName]
+                self.setCBColor(cb, v)
+                cb.setCurrentIndex(v)
+                if v != 0:
+                    s = s + v
+
+        self.cb_stage.setCurrentIndex(self.cfg['configs']['cb_stage']-1)
+        self.sb_tokens.setValue(self.cfg['configs']['tokens'])
+        self.calcTotalNeededTokens()
+        self.refreshLabels()
+        self.setWindowTitle('Great Hall Calculator - Level ' + str(s) + '/240')
+
+    def refreshLabels(self):
+        self.refreshTokenPerWin()
+        self.refreshTokensToEnd()
+        self.refreshNeededArenaWins()
+
+    def stageChangeHandler(self):
+        self.calcTotalNeededTokens()
+        self.refreshLabels()
+        self.saveData()
+
+    def refreshTokenPerWin(self):
+        stage = self.cb_stage.currentIndex()
+        self.lcdTokens.display(self.cfg['configs']['stageTokens'][stage])
+
+    def refreshTokensToEnd(self):
+        self.lcdEnd.display(self.totalTokens)
+
+    def refreshNeededArenaWins(self):
+        wins = self.totalTokens/self.getTokenPerWin()
+        self.lcdWins.display(int(math.ceil(wins)))
+
+    def getTokenPerWin(self):
+        stage = self.cb_stage.currentText()
+        return self.cfg['configs']['stageTokens'][int(stage) - 1]
+
+    def bindHandlers(self):
+        for affinity in self.cfg['configs']['sections']:
+            for cbId in range(6):
+                cb = self.findChild(QComboBox, "cb_" + affinity + "_" + str(cbId+1))
+                cb.currentIndexChanged.connect(self.stageChangeHandler)
+        self.cb_stage.currentIndexChanged.connect(self.stageChangeHandler)
+        self.sb_tokens.textChanged.connect(self.stageChangeHandler)
+
+    def subtractExistingTokens(self):
+        existingTokens = self.sb_tokens.text()
+        if existingTokens != '':
+            self.totalTokens = self.totalTokens - int(existingTokens)
+
+    def calcTotalNeededTokens(self):
+        self.totalTokens = self.cfg['configs']['maxNeededTokens']
+        for affinity in self.cfg['configs']['sections']:
+            for cbId in range(6):
+                cb = self.findChild(QComboBox, "cb_" + affinity + "_" + str(cbId+1))
+                v = self.toInt(cb.currentIndex())
+                self.setCBColor(cb, v)
+                if v > 0:
+                    self.totalTokens = self.totalTokens - self.cfg['configs']['subFromLvl'][v-1]
+        self.subtractExistingTokens()
+
+
+app = QApplication()
+frm_Main = FrmMain()
+frm_Main.show()
+app.exec()
